4cbc0b11ced1e6f37ac6ca770cef1c4c2ed41bc9
Mahedi Hasan Anik <122723875+Mahedi02@users.noreply.github.com>
2024-05-29 22:43:03 +0600

    Add files via upload

---
+++ WEEK 10/W10-THURSDAY/Sliding_Window_Median.cpp
@@ -0,0 +1,68 @@
+/* _AUTHOR_ : MAHEDI HASAN ANIK
+_CREATED_ : 2024-05-29 || 15:21:37 */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define vec vector<ll>
+#define sorta(x) sort(x.begin(), x.end());
+#define sortd(x) sort(x.begin(), x.end(), greater<int>());
+#define loop(i, s, n) for (int i = s; i < n; i++)
+#define input(x)                \
+    vec x(n);                   \
+    for (int i = 0; i < n; i++) \
+        cin >> x[i];
+#define output(x)           \
+    for (ll val : x)        \
+        cout << val << " "; \
+    cout << '\n';
+#define test  \
+    {         \
+    }         \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define even(x) (x % 2 == 0)
+#define odd(x) (x % 2 == 1)
+#define pb push_back
+#define I_AM_SPEED                \
+    ios ::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+int main()
+{
+    I_AM_SPEED;
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int l = 0, r = 0;
+    pbds<pair<int, int>> p;
+    while (r < n)
+    {
+        p.insert({a[r], r});
+        if (r - l + 1 == k)
+        {
+            int pos = k / 2;
+            if (k % 2 == 0)
+            {
+                pos--;
+            }
+            auto it = p.find_by_order(pos);
+            cout << it->first << " ";
+            p.erase({a[l], l});
+            l++;
+        }
+        r++;
+    }
+    return 0;
+}
